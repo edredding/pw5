9afa602484178031469b2674b008d5b2f3db298d
Edward Redding <edward.l.redding@gmail.com>
2022-10-30 19:53:22 -0400

    first edit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pw3'`, () => {
+  it(`should have as title 'pw'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pw3');
+    expect(app.title).toEqual('pw');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('pw3 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pw app is running!');
   });
 });
